25a2c3e5d6bb27211d7e405c1ad9af7fa6212593
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-10 22:24:31 -0700

    completed

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,23 @@
+# require 'pry'
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+  accepts_nested_attributes_for :notes
+
+  def artist_name=(name)
+    # binding.pry
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def notes=(content)
+    # binding.pry
+    content.each do |n|
+      self.notes << Note.create(content: n)
+    end
+  end
 end
